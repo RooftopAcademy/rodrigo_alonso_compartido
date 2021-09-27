0838a2612e4c732b0414e36fab492173dc5c9e68
Rodrigo Alonso <rodrigoalonso.hr@gmail.com>
2021-09-26 23:42:12 -0300

    Now home is SPA

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,21 @@ import Store from './Store';
 import Catalogue from './Catalogue';
 import Product from './Product';
 import renderProductList from './views/renderProductList';
+import renderHomeView from './views/renderHomeView';
+
+renderHomeView();
 
 let store: Store = new Store();
-const productCard: HTMLElement | null = document.getElementById("js-product-card"); //FIXME: No puedo tipar
+const productCard: HTMLElement = document.getElementById("js-product-card") as HTMLElement; //FIXME: No puedo tipar
+const homeView: HTMLElement  = document.getElementById("home-view") as HTMLElement;
+const mainTag : HTMLElement = document.getElementById("main") as HTMLElement;
+// mainTag.innerHTML = renderHomeView();
+
+homeView.addEventListener("click", (e)=>{
+    e.preventDefault();
+    mainTag.innerHTML = renderHomeView();
+})
+
 let url: string = "http://demo1372636.mockable.io/api/tina";
 
 ( async () => {
---
+++ src/views/renderHomeView.ts
@@ -0,0 +1,12 @@
+export default function renderHomeView():string{
+    return `
+    <section class="d-flex card ">
+        <img src="src/img/star.png" alt="" class="icon-main">
+        <a href="product-category.html" class="button--main">Indumentaria</a>
+    </section>
+    <section class="d-flex card ">
+        <img src="src/img/favicon-heart.png" alt="" class="icon-main">
+        <a href="product-category.html" class="button--main">Accesorios</a>
+    </section> 
+    `
+}
